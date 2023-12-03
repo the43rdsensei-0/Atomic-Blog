6e2c0387cf56d7b674cf61496bba88be689d0ae2
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-12-03 13:44:30 +0100

    optimized the app performance by memoizing the value broadcasted by the postContext provider

--- src/PostProvider.js
+++ src/PostProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -27,24 +27,22 @@ function PostProvider({ children }) {
     setPosts([]);
   }
 
-  return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onClearPosts: handleClearPosts,
-        onAddPost: handleAddPost,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
-  );
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onClearPosts: handleClearPosts,
+      onAddPost: handleAddPost,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchQuery, searchedPosts]);
+
+  return <PostContext.Provider value={value}>{children}</PostContext.Provider>;
 }
 
 function usePosts() {
   const context = useContext(PostContext);
-  if(context === undefined) throw new Error('PostContext was used outside the PostProvider')
+  if (context === undefined) throw new Error("PostContext was used outside the PostProvider");
   return context;
 }
 
